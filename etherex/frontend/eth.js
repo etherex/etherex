b44e630e287ba2c05713b66350a6f9e8a9f51b26
Vincent Gariepy <caktux@gmail.com>
2014-06-06 14:27:53 -0400

    cleanup addressbook, namereg and tweaks

--- etherex/frontend/eth.js
+++ etherex/frontend/eth.js
@@ -15,44 +15,46 @@ else if (typeof(String.prototype.pad) === "undefined")
 }
 
 var spec = [
-  { "method": "procedures", "params": null, "order": [], "returns": [] },
-  { "method": "coinbase", "params": null, "order": [], "returns" : "" },
-  { "method": "isListening", "params": null, "order": [], "returns" : false },
-  { "method": "isMining", "params": null, "order": [], "returns" : false },
-  { "method": "gasPrice", "params": null, "order": [], "returns" : "" },
-  { "method": "key", "params": null, "order": [], "returns" : "" },
-  { "method": "keys", "params": null, "order": [], "returns" : [] },
-  { "method": "peerCount", "params": null, "order": [], "returns" : 0 },
-  { "method": "balanceAt", "params": { "a": "" }, "order": ["a"], "returns" : "" },
-  { "method": "storageAt", "params": { "a": "", "x": "" }, "order": ["a", "x"], "returns" : "" },
-  { "method": "txCountAt", "params": { "a": "" },"order": ["a"], "returns" : "" },
-  { "method": "isContractAt", "params": { "a": "" }, "order": ["a"], "returns" : false },
-  { "method": "create", "params": { "sec": "", "xEndowment": "", "bCode": "", "xGas": "", "xGasPrice": "" }, "order": ["sec", "xEndowment", "bCode", "xGas", "xGasPrice"] , "returns": {} },
-  { "method": "transact", "params": { "sec": "", "xValue": "", "aDest": "", "bData": "", "xGas": "", "xGasPrice": "" }, "order": ["sec", "xValue", "aDest", "bData", "xGas", "xGasPrice"], "returns": {} },
-  { "method": "secretToAddress", "params": { "a": "" }, "order": ["a"], "returns" : "" }
+            { "method": "procedures", "params": null, "order": [], "returns": [] },
+            { "method": "coinbase", "params": null, "order": [], "returns" : "" },
+            { "method": "isListening", "params": null, "order": [], "returns" : false },
+            { "method": "isMining", "params": null, "order": [], "returns" : false },
+            { "method": "gasPrice", "params": null, "order": [], "returns" : "" },
+            { "method": "key", "params": null, "order": [], "returns" : "" },
+            { "method": "keys", "params": null, "order": [], "returns" : [] },
+            { "method": "peerCount", "params": null, "order": [], "returns" : 0 },
+            { "method": "balanceAt", "params": { "a": "" }, "order": ["a"], "returns" : "" },
+            { "method": "storageAt", "params": { "a": "", "x": "" }, "order": ["a", "x"], "returns" : "" },
+            { "method": "txCountAt", "params": { "a": "" },"order": ["a"], "returns" : "" },
+            { "method": "isContractAt", "params": { "a": "" }, "order": ["a"], "returns" : false },
+            { "method": "create", "params": { "sec": "", "xEndowment": "", "bCode": "", "xGas": "", "xGasPrice": "" }, "order": ["sec", "xEndowment", "bCode", "xGas", "xGasPrice"] , "returns": "" },
+            { "method": "transact", "params": { "sec": "", "xValue": "", "aDest": "", "bData": "", "xGas": "", "xGasPrice": "" }, "order": ["sec", "xValue", "aDest", "bData", "xGas", "xGasPrice"], "returns": {} },
+            { "method": "secretToAddress", "params": { "a": "" }, "order": ["a"], "returns" : "" },
+            { "method": "lll", "params": { "s": "" }, "order": ["s"], "returns" : "" }
 ];
 
 window.eth = (function ethScope() {
 	var m_reqId = 0
 	var ret = {}
+    function reformat(m, d) { return m == "lll" ? d.bin() : d; }
 	function reqSync(m, p) {
 		var req = { "jsonrpc": "2.0", "method": m, "params": p, "id": m_reqId }
 		m_reqId++
 		var request = new XMLHttpRequest();	
-		request.open("POST", "http://localhost:8080", false);
+        request.open("POST", "http://localhost:8080", false)
 //		console.log("Sending " + JSON.stringify(req))
-		request.send(JSON.stringify(req));
-		return JSON.parse(request.responseText).result;
+        request.send(JSON.stringify(req))
+        return reformat(m, JSON.parse(request.responseText).result)
 	}
 	function reqAsync(m, p, f) {
 		var req = { "jsonrpc": "2.0", "method": m, "params": p, "id": m_reqId }
 		m_reqId++
 		var request = new XMLHttpRequest();	
-		request.open("POST", "http://localhost:8080", true);
-		request.send(JSON.stringify(req));
+        request.open("POST", "http://localhost:8080", true)
+        request.send(JSON.stringify(req))
 		request.onreadystatechange = function() {
 			if (request.readyState === 4)
-	        	f(JSON.parse(request.responseText).result)
+                f(reformat(m, JSON.parse(request.responseText).result))
 	    };
 	}
 	function isEmpty(obj) {
@@ -73,14 +75,19 @@ window.eth = (function ethScope() {
 				p[s.order[j]] = (s.order[j][0] === "b") ? a[j].unbin() : a[j]
 			return p
 		};
-		ret[am] = function() { return reqAsync(m, getParams(arguments), arguments[s.order.length]) }
-		if (s.params)
-			ret[m] = function() { return reqSync(m, getParams(arguments)) }
+        if (m == "create" || m == "transact")
+            ret[m] = function() { return reqAsync(m, getParams(arguments), arguments[s.order.length]) }
 		else
-			Object.defineProperty(ret, m, {
-				get: function() { return reqSync(m, {}); },
-				set: function(v) {}
+		{
+			ret[am] = function() { return reqAsync(m, getParams(arguments), arguments[s.order.length]) }
+			if (s.params)
+				ret[m] = function() { return reqSync(m, getParams(arguments)) }
+            else
+				Object.defineProperty(ret, m, {
+					get: function() { return reqSync(m, {}); },
+					set: function(v) {}
 			})
+        }
 	})(spec[si]);
 
 	ret.check = function(force) {
--- etherex/frontend/scripts.js
+++ etherex/frontend/scripts.js
@@ -5,24 +5,30 @@
   EtherEx.coinbase = "0x995db8d9f8f4dcc2b35da87a3768bd10eb8ee2da";
 
   EtherEx.addresses = {
-    namereg: "0x2d0aceee7e5ab874e22ccf8d1a649f59106d74e8",
+    namereg: "0x2d0aceee7e5ab874e22ccf8d1a649f59106d74e8", // "8b01a7e2317fbb6d8096bb667d0604ce898aeaf8"
     trades: "0x6b385354b319a439b36bbeb74f8b8c0517b359ad",
-    xeth: "0x5620133321fcac7f15a5c570016f6cb6dc263f9d",
     markets: "0x5620133321fcac7f15a5c570016f6cb6dc263f9d"
   };
 
-  EtherEx.markets = {};
+  EtherEx.markets = [
+    {},
+    {
+      name: "XETH/ETH",
+      address: "0xe559de5527492bcb42ec68d07df0742a98ec3f1e",
+      minamount: Ethereum.BigInteger("10").pow(18),
+      minprice: Ethereum.BigInteger("10").pow(8),
+    }
+  ];
 
   EtherEx.loadMarkets = function() {
     var last = eth.storageAt(EtherEx.addresses.markets, String(2));
-    for (var i = 100; i < 100 + parseInt(last) / 1000000; i = i + 5) {
+    for (var i = 100; i < 100 + parseInt(last) / 10000000; i = i + 5) {
       var id = eth.storageAt(EtherEx.addresses.markets, String(i+4)).dec();
       EtherEx.markets[id] = {
-        'id': id,
-        'name': eth.storageAt(EtherEx.addresses.markets, String(i)),
-        'address': eth.storageAt(EtherEx.addresses.markets, String(i+3)),
-        'minamount': eth.storageAt(EtherEx.addresses.markets, String(i+1)).dec(),
-        'minprice': eth.storageAt(EtherEx.addresses.markets, String(i+2)).dec(),
+        name: eth.storageAt(EtherEx.addresses.markets, String(i)),
+        address: eth.storageAt(EtherEx.addresses.markets, String(i+3)),
+        minamount: eth.storageAt(EtherEx.addresses.markets, String(i+1)).dec(),
+        minprice: eth.storageAt(EtherEx.addresses.markets, String(i+2)).dec(),
       }
     };
     // console.log(EtherEx.markets);
@@ -36,13 +42,34 @@
     return eth.storageAt(EtherEx.addresses.namereg, "0x" + _a).bin().unpad();
   };
 
+  EtherEx.sendETH = function() {
+    eth.transact(
+      eth.key,
+      String(Ethereum.BigInteger($("#value").val()).multiply(Ethereum.BigInteger("10").pow(18))),
+      "0x" + $("#to").val(),
+      "",
+      "500",
+      eth.gasPrice
+    );
+  };
+
   EtherEx.sendXETH = function() {
     eth.transact(
       eth.key,
       "0",
-      EtherEx.addresses.xeth,
+      EtherEx.markets[1].address,
       eth.secretToAddress(document.getElementById("to").value).pad(32) + document.getElementById("value").value.pad(32),
-      "10000",
+      "1000",
+      eth.gasPrice
+    );
+  };
+
+  EtherEx.create = function() {
+    eth.create(
+      eth.key,
+      $("#endowment").val(),
+      $("#code").val(),
+      $("#gas").val(),
       eth.gasPrice
     );
   };
@@ -135,22 +162,37 @@
     var err = $('<a class="error" href="#"><i class="icon-cancel" title="Error - try reloading"></i></a>');
 
     try {
+      // Cleanup
       $(".error").remove();
+      $("#addressbook > table > tbody tr").remove();
+
+      EtherEx.loadMarkets();
 
       document.getElementById("eth").innerHTML = Ethereum.BigInteger(eth.balanceAt(eth.coinbase).dec()).divide(Ethereum.BigInteger("10").pow(18));
 
-      document.getElementById("xeth").innerHTML = eth.storageAt(EtherEx.addresses.xeth, eth.coinbase).dec();
+      document.getElementById("xeth").innerHTML = eth.storageAt(EtherEx.markets[1].address, eth.coinbase).dec();
       document.getElementById("tot").innerHTML = eth.balanceAt(EtherEx.coinbase).dec();
 
       var keys = eth.keys;
+
       for (var i = keys.length - 1; i >= 0; i--) {
         var key = eth.secretToAddress(keys[i]);
         var bal = Ethereum.BigInteger(eth.balanceAt(key).dec()).divide(Ethereum.BigInteger("10").pow(18));
         var entry = $("<tr><td><div>" + key.substr(2) + "</div></td><td>" + bal + " ETH</td></tr>");
 
         $("#addressbook > table > tbody").append(entry);
+
+        try {
+          for (var m = EtherEx.markets.length - 1; m >= 1; m--) {
+            var subbal = eth.storageAt(EtherEx.markets[m].address, key).dec();
+            var subentry = $('<tr><td class="sub">+ ' + EtherEx.markets[m].name + '</td><td>' + subbal + " </td></tr>");
+
+            $("#addressbook > table > tbody").append(subentry);
+          };
+        }
+        catch (er) {}
       };
-      EtherEx.loadMarkets();
+
       EtherEx.getOrderbook();
     }
     catch (e) {
@@ -184,6 +226,24 @@
       this.addr = EtherEx.getAddress($(this).val());
       if (this.addr.length == 40)
         $(this).val(this.addr);
+
+      this.namereg = EtherEx.getName($(this).val());
+      if (this.namereg.length > 0) {
+        $(this).css('color', 'rgba(0,0,0,.15)');
+        $("#toname").html(this.namereg);
+      }
+      else {
+        $(this).css('color', 'rgba(0,0,0)');
+        $("#toname").html("");
+      }
+    });
+    $("#to").on('focus', function() {
+      $(this).css('color', 'rgba(0,0,0)');
+      $("#toname").html("");
+    });
+
+    $("#toname").on('click', function() {
+      $("#to").focus();
     });
 
     $("#addr").on('keyup', function(e) {
@@ -192,6 +252,16 @@
         $("#addrResult").html(this.namereg);
       else
         $("#addrResult").html("");
+    });
+
+    $("#sendeth").on('click', function() {
+      if ($("#to").val() == "") {
+        alert("Please provide a recipient address.");
+        return;
+      }
+      if (window.confirm("Send " + $("#value").val() + " ETH to " + $("#to").val() + " ?")) {
+        EtherEx.sendETH();
+      }
     });
 
     $("#sendxeth").on('click', function() {
@@ -204,13 +274,24 @@
       }
     });
 
+    $("#create").on('click', function() {
+      if ($("#code").val() == "") {
+        alert("Please provide the compiled contract code.");
+        return;
+      }
+      if (window.confirm("Create contract with " + $("#gas").val() + " gas?")) {
+        EtherEx.create();
+      }
+    });
+
     $("#refresh, #clear").on('click', function() {
       EtherEx.clear();
       EtherEx.updateBalances();
     });
 
     EtherEx.updateBalances();
-    eth.watch(EtherEx.addresses.xeth, eth.coinbase, EtherEx.updateBalances);
+    $("#to").trigger('keyup');
+    eth.watch(EtherEx.markets[1].address, eth.coinbase, EtherEx.updateBalances);
   });
 
 })(jQuery);
