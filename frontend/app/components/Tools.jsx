0dfbae8f30126d7a69defc7e0b28a8af710e3dda
caktux <caktux@gmail.com>
2015-02-18 23:22:25 -0500

    move deposit, withdraw and send to wallet section

--- frontend/app/components/Tools.jsx
+++ frontend/app/components/Tools.jsx
@@ -9,9 +9,6 @@ var FluxMixin = Fluxxor.FluxMixin(React);
 var AlertDismissable = require('./AlertDismissable');
 
 var SubRegister = require('./SubRegister');
-var SubDeposit = require('./SubDeposit');
-var SubWithdraw = require('./SubWithdraw');
-var SubSend = require('./SubSend');
 
 var TradeList = React.createClass({
   mixins: [FluxMixin],
@@ -27,46 +24,6 @@ var TradeList = React.createClass({
     return (
       <div>
         <AlertDismissable ref="alerts" level={this.state.alertLevel} message={this.state.alertMessage} />
-
-        <div className="container-fluid">
-          <div className="col-md-6">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">Deposit</h3>
-              </div>
-              <div className="panel-body">
-                <div className="container-fluid">
-                  <SubDeposit market={this.props.market.market} />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">Withdraw</h3>
-              </div>
-              <div className="panel-body">
-                <div className="container-fluid">
-                  <SubWithdraw market={this.props.market.market} user={this.props.user.user} />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="container-fluid col-md-4 col-md-offset-4">
-          <div className="panel panel-default">
-            <div className="panel-heading">
-              <h3 className="panel-title">Send</h3>
-            </div>
-            <div className="panel-body">
-              <div className="container-fluid">
-                <SubSend market={this.props.market.market} />
-              </div>
-            </div>
-          </div>
-        </div>
 
         <div className="container col-md-6 col-md-offset-3">
           <div className="panel panel-default">
--- frontend/app/components/Wallet.jsx
+++ frontend/app/components/Wallet.jsx
@@ -4,6 +4,9 @@ var React = require("react");
 var Fluxxor = require("fluxxor");
 var FluxMixin = Fluxxor.FluxMixin(React);
 
+var SubSend = require('./SubSend');
+var SubDeposit = require('./SubDeposit');
+var SubWithdraw = require('./SubWithdraw');
 // var MarketFilter = require("./MarketFilter"); TODO
 var MarketList = require("./MarketList");
 var TxsList = require("./TxsList");
@@ -16,6 +19,48 @@ var Markets = React.createClass({
     // <MarketFilter market={this.props.market} trades={this.props.trades} user={this.props.user} />
     return (
       <div>
+
+        <div className="container-fluid">
+          <div className="col-md-6">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title">Withdraw {this.props.market.market.name}</h3>
+              </div>
+              <div className="panel-body">
+                <div className="container-fluid">
+                  <SubWithdraw market={this.props.market.market} user={this.props.user.user} />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title">Deposit {this.props.market.market.name}</h3>
+              </div>
+              <div className="panel-body">
+                <div className="container-fluid">
+                  <SubDeposit market={this.props.market.market} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid">
+          <div className="col-md-4 col-md-offset-4">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title">Send {this.props.market.market.name}</h3>
+              </div>
+              <div className="panel-body">
+                <div className="container-fluid">
+                  <SubSend market={this.props.market.market} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="container-fluid">
           <div className="row">
             {!this.props.market.error &&
