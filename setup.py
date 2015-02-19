3cceb625357cc19cc4a02c6975b0c439bba274a8
Joris Bontje <jorisbontje@gmail.com>
2015-02-19 16:44:32 +0100

    travis setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='etherex',
+      version='1.0',
+      description='A Decentralized Future Calls For A Decentralized Exchange',
+      author='caktux',
+      author_email='caktux@gmail.com',
+      url='https://github.com/etherex/etherex/')
